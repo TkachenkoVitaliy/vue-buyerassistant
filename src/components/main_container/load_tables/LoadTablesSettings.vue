0894add8e512eced12e79c76fc34a24288a83988
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2022-08-03 18:36:02 +0500

    Added LoadTablesSettings

---
+++ src/components/main_container/load_tables/LoadTablesSettings.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>
+          <v-autocomplete
+              :items='months'
+              v-model='settings.month'
+              v-on:change='updateSettings'
+          ></v-autocomplete>
+        </td>
+        <td>
+          <v-autocomplete
+              :items='years'
+              v-model='settings.year'
+              v-on:change='updateSettings'
+          ></v-autocomplete>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['settings'],
+    data() {
+      return {
+        months: [{text: 'Январь', value: 1}, {text: 'Февраль', value: 2}, {text: 'Март', value: 3},
+          {text: 'Апрель', value: 4}, {text: 'Май', value: 5}, {text: 'Июнь', value: 6},
+          {text: 'Июль', value: 7}, {text: 'Август', value: 8}, {text: 'Сентябрь', value: 9},
+          {text: 'Октябрь', value: 10}, {text: 'Ноябрь', value: 11}, {text: 'Декабрь', value: 12}],
+        years: []
+      }
+    },
+    methods: {
+      setYears() {
+        let currentYear = new Date().getFullYear()
+        this.years = [currentYear - 1, currentYear, currentYear + 1]
+      },
+      updateSettings() {
+        console.log(this.settings)
+        this.$emit('updateSettings')
+      }
+    },
+    beforeMount() {
+      this.setYears()
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
--- src/views/app/LoadTables.vue
+++ src/views/app/LoadTables.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <LoadTablesSettings
+        v-bind:settings = 'userSettings'
+        v-on:updateSettings = 'postUserSettings'
+    />
     <FactoriesLoadTables
         v-bind:loadTables = 'loadTables'
     />
@@ -7,14 +11,16 @@
 </template>
 
 <script>
-  import FactoriesLoadTables from "@/components/LoadTables/FactoriesLoadTables";
-  import axios from "axios";
+  import axios from "axios"
+  import FactoriesLoadTables from "@/components/main_container/load_tables/FactoriesLoadTables"
+  import LoadTablesSettings from "@/components/main_container/load_tables/LoadTablesSettings";
 
   export default {
-    components: {FactoriesLoadTables},
+    components: {FactoriesLoadTables, LoadTablesSettings},
     data() {
       return {
-        loadTables: []
+        loadTables: [],
+        userSettings: null
       }
     },
     methods: {
@@ -24,10 +30,36 @@
         }).catch(() => {
           alert('Не удалось получить таблицы шахматки для филиалов')
         })
+      },
+      getUserSettings() {
+        axios.get('http://localhost:8081/loadTables/settings').then((response) => {
+          this.userSettings = response.data
+        }).catch(() => {
+          alert('Не удалось получить настройки шахматки')
+        })
+      },
+      postUserSettings() {
+        console.log('post')
+        console.log(this.userSettings)
+        axios({
+          url: 'http://localhost:8081/loadTables/settings',
+          method: 'post',
+          data: {
+            id: 1,
+            username: 'admin',
+            month: 3,
+            year: 2022
+          }
+        }).then((response) => {
+          this.userSettings = response.data
+        }).catch(() => {
+          alert('При сохранении произошла ошибка')
+        })
       }
     },
     mounted() {
       this.getLoadTables()
+      this.getUserSettings()
     }
   }
 
